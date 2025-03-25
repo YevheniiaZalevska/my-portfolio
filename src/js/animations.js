7d6dfef289ecf4edd234ff9db90a328d123e1c83
YevheniiaZalevska <yevhenia05@gmail.com>
2025-03-25 19:03:07 +0100

    Create animations.js

---
+++ src/js/animations.js
@@ -0,0 +1,22 @@
+//  About Me
+ScrollReveal().reveal('.about-me-text', {
+  delay: 200,
+  distance: '30px',
+  duration: 800,
+  origin: 'bottom'
+});
+
+ScrollReveal().reveal('.about-tech-content', {
+  delay: 300,
+  distance: '40px',
+  duration: 900,
+  origin: 'left',
+  interval: 200
+});
+
+ScrollReveal().reveal('.list2', {
+  delay: 400,
+  distance: '40px',
+  duration: 1000,
+  origin: 'right'
+});
