94dde42e354a11e5314b122ce24b38f17c800780
YevheniiaZalevska <yevhenia05@gmail.com>
2025-03-26 17:44:51 +0100

    Update translations.js

--- src/js/lang/translations.js
+++ src/js/lang/translations.js
@@ -1,4 +1,35 @@
 export const translations = {
-  en: { home: "Home", about: "About" },
-  ua: { home: "Головна", about: "Про нас" }
+  en: {
+    skills: "My skills",
+    projects: "Projects",
+    education: "Education",
+    contacts: "Contacts",
+    about_text: "Hi, . ",
+    hero_name: "Yevheniia Zalevska",
+    about_text: "I like solving problems and looking for different solutions. My goal is to constantly improve my skills and create valuable digital products. I am inspired to work on new projects and gain experience. I have sound knowledge of HTML, CSS, JavaScript, and React. Also, I have hands-on experience in following Agile/Scrum methodology when working in a team. I am a fast learner, responsible, and ready for challenging tasks. In my free time I like to dance and paint. I am a creative person and always have a positive attitude towards new tasks.",
+
+    
+    soft: "Soft Skills",
+    languages: "Languages",
+    certificates: "Certificates",
+    downloadCV: "Download CV",
+    live: "Live page",
+    code: "View Code"
+  },
+
+  ua: {
+    home: "Головна",
+    about: "Про мене",
+    projects: "Проєкти",
+    education: "Освіта",
+    contacts: "Контакти",
+    skills: "Навички",
+    tools: "Інструменти",
+    soft: "Гнучкі навички",
+    languages: "Мови",
+    certificates: "Сертифікати",
+    downloadCV: "Завантажити CV",
+    live: "Переглянути",
+    code: "Код"
+  }
 };
