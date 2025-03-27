af33398945f48f7c500121766c33b661c10b0aea
YevheniiaZalevska <yevhenia05@gmail.com>
2025-03-27 12:04:39 +0100

    Update translations.js

--- src/js/lang/translations.js
+++ src/js/lang/translations.js
@@ -1,12 +1,13 @@
+// aboutElement.innerHTML = translations[lang].about_text;
+
 export const translations = {
   en: {
     skills: "My skills",
     projects: "Projects",
     education: "Education",
     contacts: "Contacts",
-    about_text: "Hi, . ",
-    hero_name: "Yevheniia Zalevska",
-    about_text: "I like solving problems and looking for different solutions. My goal is to constantly improve my skills and create valuable digital products. I am inspired to work on new projects and gain experience. I have sound knowledge of HTML, CSS, JavaScript, and React. Also, I have hands-on experience in following Agile/Scrum methodology when working in a team. I am a fast learner, responsible, and ready for challenging tasks. In my free time I like to dance and paint. I am a creative person and always have a positive attitude towards new tasks.",
+     hero_name: "Yevheniia Zalevska",
+    about_text: 'Hi, I’m <strong class="hero-name" data-lang="hero_name">Yevheniia Zalevska</strong>. I like solving problems and looking for different solutions. My goal is to constantly improve my skills and create valuable digital products. I am inspired to work on new projects and gain experience. I have sound knowledge of HTML, CSS, JavaScript, and React. Also, I have hands-on experience in following Agile/Scrum methodology when working in a team. I am a fast learner, responsible, and ready for challenging tasks. In my free time I like to dance and paint. I am a creative person and always have a positive attitude towards new tasks.',
 
     
     soft: "Soft Skills",
