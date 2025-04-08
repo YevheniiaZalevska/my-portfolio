391f4114fc0a968a66cf0c0dab7886d33bad6128
YevheniiaZalevska <yevhenia05@gmail.com>
2025-04-08 19:31:11 +0200

    Create animationss.js

---
+++ src/js/animationss.js
@@ -0,0 +1,41 @@
+//  About Me
+ScrollReveal().reveal('.list2', {
+  delay: 400,
+  distance: '40px',
+  duration: 1000,
+  origin: 'right'
+});
+
+ScrollReveal().reveal('.about-tech-content', {
+  delay: 300,
+  distance: '40px',
+  duration: 900,
+  origin: 'left',
+  interval: 200
+});
+
+
+// Education Section
+ScrollReveal().reveal('.education-title', {
+  delay: 200,
+  distance: '30px',
+  duration: 800,
+  origin: 'top'
+});
+
+ScrollReveal().reveal('.education-item', {
+  delay: 300,
+  distance: '40px',
+  duration: 900,
+  origin: 'bottom',
+  interval: 200
+});
+
+
+// Certificates Section
+ScrollReveal().reveal('.cert-btn-content', {
+  delay: 300,
+  distance: '40px',
+  duration: 900,
+  origin: 'bottom'
+});
